8dbced0cdc3d9ad5e360173ccc56d3b473449629
AllanMuhari <allanmuhari087@gmail.com>
2024-06-09 00:47:50 +0300

    show only 6 destinations in home page

--- src/pages/Home/Featureddestinations.jsx
+++ src/pages/Home/Featureddestinations.jsx
@@ -3,15 +3,20 @@ import Destinations from "../../assets/Data/data";
 import Featureddestination from "./Featureddestination";
 
 const Featureddestinations = () => {
-  const [displayedDestinations, setDisplayedDestinations] = useState(3);
+  const initialDisplayCount = 2;
+  const [displayedDestinations, setDisplayedDestinations] =
+    useState(initialDisplayCount);
 
   const loadMore = () => {
-    setDisplayedDestinations(displayedDestinations + 3);
+    setDisplayedDestinations((prevCount) =>
+      Math.min(prevCount + 2, Destinations.length)
+    );
   };
 
   return (
-    <div>
-      <h1 className="text-center font-extrabold text-3xl underline">
+    <div className="mt-[7rem] px-4">
+      {" "}
+      <h1 className="text-center font-extrabold text-3xl underline mb-6">
         Featured Destinations
       </h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 w-[80%] m-auto mb-7">
@@ -26,7 +31,7 @@ const Featureddestinations = () => {
           )
         )}
       </div>
-      {displayedDestinations < Destinations.length && (
+      {displayedDestinations < 6 && (
         <button
           onClick={loadMore}
           className="bg-blue-500 text-white px-4 py-2 rounded-lg shadow hover:bg-blue-600 flex m-auto mb-4 justify-center content-center">
